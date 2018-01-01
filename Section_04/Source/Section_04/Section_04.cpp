22a5a4b2511de40748df2cd12d28e9a466fc4b9c
A408889 <A408889@MC0ZUBGC>
2018-01-01 18:36:04 +0100

    no message

--- Section_04/Source/Section_04/Section_04.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Section_04.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Section_04, "Section_04" );
