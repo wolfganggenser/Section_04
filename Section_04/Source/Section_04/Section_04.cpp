afab182cf6113fa842245fde4185e03ce34fcb8c
A408889 <A408889@MC0ZUBGC>
2018-01-01 16:10:54 +0100

    Start of UE Project, move gitignore to Folder Section_04 because default gitignore would not ignore intermediate files

---
+++ Section_04/Source/Section_04/Section_04.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Section_04.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Section_04, "Section_04" );
